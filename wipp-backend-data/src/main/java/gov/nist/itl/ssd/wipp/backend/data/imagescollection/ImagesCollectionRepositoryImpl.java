93e7ed8cd4de17d59ca865b283671eb55e1accfa
Samiasa <samia.benjida@telecomnancy.net>
2019-03-15 16:04:10 -0400

    feat: renamed wipp-backend-images to wipp-backend-data
    BREAKING CHANGE: package name change for ImagesCollection, Image and MetadataFile requires update of _class field in all documents of collections imagesCollection, image and metadataFile in database

---
+++ wipp-backend-data/src/main/java/gov/nist/itl/ssd/wipp/backend/data/imagescollection/ImagesCollectionRepositoryImpl.java
@@ -0,0 +1,133 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of
+ * their official duties. Pursuant to title 17 Section 105 of the United
+ * States Code this software is not subject to copyright protection and is
+ * in the public domain. This software is an experimental system. NIST assumes
+ * no responsibility whatsoever for its use by other parties, and makes no
+ * guarantees, expressed or implied, about its quality, reliability, or
+ * any other characteristic. We would appreciate acknowledgement if the
+ * software is used.
+ */
+package gov.nist.itl.ssd.wipp.backend.data.imagescollection;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+
+import gov.nist.itl.ssd.wipp.backend.data.imagescollection.images.Image;
+import gov.nist.itl.ssd.wipp.backend.data.imagescollection.metadatafiles.MetadataFile;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+/**
+ *
+ * @author Antoine Vandecreme
+ */
+public class ImagesCollectionRepositoryImpl
+        implements ImagesCollectionRepositoryCustom {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public void updateImagesCaches(String imagesCollectionId) {
+    	MongoCollection<Document> collection = mongoTemplate.getCollection(
+                mongoTemplate.getCollectionName(Image.class));
+
+    	List<Document> pipeline = Arrays.asList(
+                new Document("$match",
+                        new Document("imagesCollection",
+                                imagesCollectionId)),
+                new Document("$group",
+                		new Document()
+	                        .append("_id", "aggregation")
+	                        .append("numberOfImages", new BasicDBObject("$sum", 1))
+	                        .append("imagesTotalSize",
+	                                new Document("$sum", "$fileSize"))
+	                        .append("numberOfImportingImages",
+	                                new Document("$sum",
+	                                        new Document("$cond",
+	                                                Arrays.asList(
+	                                                        "$importing",
+	                                                        1,
+	                                                        0))))
+	                        .append("numberOfImportErrors",
+	                                new Document("$sum",
+	                                        new Document("$cond",
+	                                                Arrays.asList(
+	                                                        "$importError",
+	                                                        1,
+	                                                        0))))
+				));
+    	AggregateIterable<Document> output = collection.aggregate(pipeline);
+        int numberOfImages = 0;
+        long imagesTotalSize = 0;
+        int numberOfImportingImages = 0;
+        int numberOfImportErrors = 0;
+
+        Iterator<Document> iterator = output.iterator();
+
+        if (iterator.hasNext()) {
+        	Document dbo = iterator.next();
+            numberOfImages = dbo.getInteger("numberOfImages");
+            imagesTotalSize = dbo.getLong("imagesTotalSize");
+            numberOfImportingImages = dbo.getInteger("numberOfImportingImages");
+            numberOfImportErrors = dbo.getInteger("numberOfImportErrors");
+        }
+
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("id").is(imagesCollectionId)),
+                new Update()
+                .set("numberOfImages", numberOfImages)
+                .set("imagesTotalSize", imagesTotalSize)
+                .set("numberImportingImages", numberOfImportingImages)
+                .set("numberOfImportErrors", numberOfImportErrors),
+                ImagesCollection.class);
+    }
+
+    @Override
+    public void updateMetadataFilesCaches(String imagesCollectionId) {
+    	MongoCollection<Document> collection = mongoTemplate.getCollection(
+                mongoTemplate.getCollectionName(MetadataFile.class));
+
+    	List<Document> pipeline = Arrays.asList(
+                new Document("$match",
+                        new Document("imagesCollection",
+                                imagesCollectionId)),
+                new Document("$group",
+                		new Document()
+	                		.append("_id", "aggregation")
+	                        .append("numberOfMetadataFiles",
+	                                new Document("$sum", 1))
+	                        .append("metadataFilesTotalSize",
+	                                new Document("$sum", "$fileSize"))
+                ));
+    	AggregateIterable<Document> output = collection.aggregate(pipeline);
+
+    	int numberOfMetadataFiles = 0;
+        long metadataFilesTotalSize = 0;
+        Iterator<Document> iterator = output.iterator();
+        if (iterator.hasNext()) {
+        	Document dbo = iterator.next();
+            numberOfMetadataFiles = dbo.getInteger("numberOfMetadataFiles");
+            metadataFilesTotalSize = dbo.getLong("metadataFilesTotalSize");
+        }
+        mongoTemplate.updateFirst(
+                Query.query(Criteria.where("id").is(imagesCollectionId)),
+                new Update()
+                .set("numberOfMetadataFiles", numberOfMetadataFiles)
+                .set("metadataFilesTotalSize", metadataFilesTotalSize),
+                ImagesCollection.class);
+    }
+
+}
---
+++ wipp-backend-data/src/main/java/gov/nist/itl/ssd/wipp/backend/data/imagescollection/ImagesCollectionResourceProcessor.java
@@ -0,0 +1,72 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of
+ * their official duties. Pursuant to title 17 Section 105 of the United
+ * States Code this software is not subject to copyright protection and is
+ * in the public domain. This software is an experimental system. NIST assumes
+ * no responsibility whatsoever for its use by other parties, and makes no
+ * guarantees, expressed or implied, about its quality, reliability, or
+ * any other characteristic. We would appreciate acknowledgement if the
+ * software is used.
+ */
+package gov.nist.itl.ssd.wipp.backend.data.imagescollection;
+
+import gov.nist.itl.ssd.wipp.backend.core.rest.PaginationParameterTemplatesHelper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Add link to images and metadata files to an imagesCollection
+ *
+ * @author Antoine Vandecreme <antoine.vandecreme at nist.gov>
+ */
+@Component
+public class ImagesCollectionResourceProcessor
+        implements ResourceProcessor<Resource<ImagesCollection>> {
+
+    @Autowired
+    private PaginationParameterTemplatesHelper assembler;
+
+    @Autowired
+    private EntityLinks entityLinks;
+
+    @Override
+    public Resource<ImagesCollection> process(
+            Resource<ImagesCollection> resource) {
+        ImagesCollection imagesCollection = resource.getContent();
+
+        Link imagesLink = entityLinks.linkForSingleResource(
+                ImagesCollection.class, imagesCollection.getId())
+                .slash("images")
+                .withRel("images");
+        resource.add(assembler.appendPaginationParameterTemplates(imagesLink));
+
+        Link metadataFileLink = entityLinks.linkForSingleResource(
+                ImagesCollection.class, imagesCollection.getId())
+                .slash("metadataFiles")
+                .withRel("metadataFiles");
+        resource.add(assembler.appendPaginationParameterTemplates(
+                metadataFileLink));
+
+        Link downloadLink = linkTo(ImagesCollectionDownloadController.class,
+                imagesCollection.getId())
+                .withRel("download");
+        resource.add(downloadLink);
+
+        Link copyLink = linkTo(ImagesCollectionCopyController.class,
+                imagesCollection.getId())
+                .withRel("copy");
+        resource.add(copyLink);
+
+        return resource;
+    }
+
+}
