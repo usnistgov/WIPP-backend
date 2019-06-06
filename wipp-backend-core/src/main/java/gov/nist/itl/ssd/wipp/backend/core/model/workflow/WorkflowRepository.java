12070891c3e0e5109626c145270a0e72c2a8f5cb
mno <mohamed.ouladi@nist.gov>
2019-06-06 17:29:53 -0400

    feat: Added findByNameContainingIgnoreCase to filter workflows by name

--- wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/model/workflow/WorkflowRepository.java
+++ wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/model/workflow/WorkflowRepository.java
@@ -3,13 +3,19 @@
  */
 package gov.nist.itl.ssd.wipp.backend.core.model.workflow;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
 
 /**
  * @author Mylene Simon <mylene.simon at nist.gov>
+ * @author Mohamed Ouladi <mohamed.ouladi at nist.gov>
  *
  */
 public interface WorkflowRepository extends MongoRepository<Workflow, String> {
 
+	Page<Workflow> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
 
 }
