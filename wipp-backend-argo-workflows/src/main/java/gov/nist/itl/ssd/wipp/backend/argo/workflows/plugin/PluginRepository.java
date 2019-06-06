271d8373f0dcdf7be3db32f73301fcb0a2b065f5
mno <mohamed.ouladi@nist.gov>
2019-06-06 17:21:38 -0400

    feat: Added findByNameContainingIgnoreCase to filter plugins by name

--- wipp-backend-argo-workflows/src/main/java/gov/nist/itl/ssd/wipp/backend/argo/workflows/plugin/PluginRepository.java
+++ wipp-backend-argo-workflows/src/main/java/gov/nist/itl/ssd/wipp/backend/argo/workflows/plugin/PluginRepository.java
@@ -1,7 +1,10 @@
 package gov.nist.itl.ssd.wipp.backend.argo.workflows.plugin;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -11,9 +14,12 @@ import java.util.List;
  *
  *
  * @author Philippe Dessauw <philippe.dessauw at nist.gov>
+ * @author Mohamed Ouladi <mohamed.ouladi at nist.gov>
  */
 @RepositoryRestResource
 public interface PluginRepository extends MongoRepository<Plugin, String> {
     @Query("{}")
     List<Plugin> findCompleteList();
+    
+	Page<Plugin> findByNameContainingIgnoreCase(@Param("name") String name, Pageable p);
 }
