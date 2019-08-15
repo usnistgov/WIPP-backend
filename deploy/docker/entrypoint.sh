08900860262db36ea54c8470f3ba9e0812c2f816
Sunny <sunny22yu@gmail.com>
2019-08-15 12:20:31 -0400

    fix: add configurable kube.wippdata.pvc

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
   echo "Illegal number of parameters. Exiting..."
-  echo "Command: ./entrypoint.sh \${mongo_host} \${mongo_port}"
+  echo "Command: ./entrypoint.sh \${mongo_host} \${mongo_port} \${shared_pvc}"
   exit 1
 fi
 
 MONGO_HOST=$1
 MONGO_PORT=$2
+SHARED_PVC=$3
 
 sed -i \
   -e 's/@mongo_host@/'"${MONGO_HOST}"'/' \
   -e 's/@mongo_port@/'"${MONGO_PORT}"'/' \
+  -e 's/@shared_pvc@/'"${SHARED_PVC}"'/' \
   /opt/wipp/config/application.properties
 
 exec /usr/bin/java -jar /opt/wipp/wipp-backend.war
