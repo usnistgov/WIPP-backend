e0afffc42052784ea8458d6e9f2fef365748fc68
Samiasa <samia.benjida@nist.gov>
2019-10-07 14:58:25 -0400

    feat: add id - name - wippExecutable check at create new job

--- wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/model/job/JobEventHandler.java
+++ wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/model/job/JobEventHandler.java
@@ -1,5 +1,7 @@
 package gov.nist.itl.ssd.wipp.backend.core.model.job;
 
+import gov.nist.itl.ssd.wipp.backend.core.CoreConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
@@ -9,9 +11,27 @@ import java.util.Date;
 @Component
 @RepositoryEventHandler(Job.class)
 public class JobEventHandler {
+
+    @Autowired
+    CoreConfig config;
+
+    @Autowired
+    private JobLogic jobLogic;
+
     @HandleBeforeCreate
     public void handleBeforeCreate(Job job) {
-        job.setCreationDate(new Date());
+
+        jobLogic.assertNotNull(job.getName());
+        jobLogic.assertJobIdNull(job.getId());
+        jobLogic.assertJobNameUnique(job.getName());
+        jobLogic.assertNotNull(job.getWippExecutable());
+
+        job.setWippVersion(config.getWippVersion());
         job.setStatus(JobStatus.CREATED);
+        job.setError(null);
+        job.setCreationDate(new Date());
+        job.setStartTime(null);
+        job.setEndTime(null);
+
     }
 }
