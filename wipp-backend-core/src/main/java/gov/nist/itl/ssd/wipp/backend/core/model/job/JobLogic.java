1bc761a02d2b84f6212eac615d69f491d8c56a52
Samiasa <samia.benjida@nist.gov>
2019-10-07 14:58:25 -0400

    feat: add id - name - wippExecutable check at create new job

---
+++ wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/model/job/JobLogic.java
@@ -0,0 +1,48 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of
+ * their official duties. Pursuant to title 17 Section 105 of the United
+ * States Code this software is not subject to copyright protection and is
+ * in the public domain. This software is an experimental system. NIST assumes
+ * no responsibility whatsoever for its use by other parties, and makes no
+ * guarantees, expressed or implied, about its quality, reliability, or
+ * any other characteristic. We would appreciate acknowledgement if the
+ * software is used.
+ */
+package gov.nist.itl.ssd.wipp.backend.core.model.job;
+
+import gov.nist.itl.ssd.wipp.backend.core.rest.exception.ClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Antoine Vandecreme
+ */
+@Component
+public class JobLogic {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+
+    public void assertJobNameUnique(String name) {
+        if (jobRepository.countByName(name) != 0) {
+            throw new ClientException("A job named \""
+                    + name + "\" already exists.");
+        }
+    }
+
+    public void assertJobIdNull(String id) {
+        if (id != null) {
+            throw new ClientException(
+                    "Do not specify an id when creating a new job.");
+        }
+    }
+
+    public void assertNotNull(String name) {
+        if (name == null) {
+            throw new ClientException("You must specify a job name.");
+        }
+    }
+
+}
