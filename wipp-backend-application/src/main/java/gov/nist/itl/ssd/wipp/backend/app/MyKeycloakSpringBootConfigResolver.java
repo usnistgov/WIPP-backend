c557b05971c8c2e90829972aeb421c05939f48b4
EmilienCourt <emilien.court@telecomnancy.eu>
2019-11-13 20:37:43 +0100

    add dependencies and quick test configuration

---
+++ wipp-backend-application/src/main/java/gov/nist/itl/ssd/wipp/backend/app/MyKeycloakSpringBootConfigResolver.java
@@ -0,0 +1,28 @@
+package gov.nist.itl.ssd.wipp.backend.app;
+
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyKeycloakSpringBootConfigResolver extends KeycloakSpringBootConfigResolver {
+
+    @Autowired
+    private KeycloakSpringBootProperties properties;
+
+    private KeycloakDeployment keycloakDeployment;
+
+    @Override
+    public KeycloakDeployment resolve(HttpFacade.Request facade) {
+        if (keycloakDeployment != null) {
+            return keycloakDeployment;
+        }
+
+        keycloakDeployment = KeycloakDeploymentBuilder.build(properties);
+        return keycloakDeployment;
+    }
+}
