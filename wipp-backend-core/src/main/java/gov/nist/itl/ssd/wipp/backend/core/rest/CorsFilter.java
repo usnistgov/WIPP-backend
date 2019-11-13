40eb97e8a6e9b06215444e7546dd4ad618373c4a
EmilienCourt <emilien.court@telecomnancy.eu>
2019-11-13 19:51:31 +0100

    disable CORS for development purposes

--- wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/rest/CorsFilter.java
+++ wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/rest/CorsFilter.java
@@ -38,7 +38,7 @@ public class CorsFilter implements Filter {
 
         // For debugging purpose, we enable CORS when the request is coming
         // from localhost.
-        if (req.getRemoteAddr().equals(req.getLocalAddr())) {
+        //if (req.getRemoteAddr().equals(req.getLocalAddr())) {
             HttpServletResponse response = (HttpServletResponse) res;
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods",
@@ -46,7 +46,7 @@ public class CorsFilter implements Filter {
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers",
                     "x-requested-with, Content-Type");
-        }
+        //}
         chain.doFilter(req, res);
     }
 
