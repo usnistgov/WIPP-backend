a7ac2233682bf8b08da4bbe9da50b8f0b60db5d0
Sunny <sunny.yu@nih.gov>
2020-01-15 23:41:55 -0500

    fix: cors update

--- wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/rest/CorsFilter.java
+++ wipp-backend-core/src/main/java/gov/nist/itl/ssd/wipp/backend/core/rest/CorsFilter.java
@@ -38,15 +38,13 @@ public class CorsFilter implements Filter {
 
         // For debugging purpose, we enable CORS when the request is coming
         // from localhost.
-        if (req.getRemoteAddr().equals(req.getLocalAddr())) {
-            HttpServletResponse response = (HttpServletResponse) res;
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods",
-                    "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers",
-                    "x-requested-with, Content-Type");
-        }
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods",
+                "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "x-requested-with, Content-Type, authorization");
         chain.doFilter(req, res);
     }
 
