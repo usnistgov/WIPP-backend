c2e4c4fee314e8771bade8bc0fb0f0508800ee36
Sunny <sunny.yu@nih.gov>
2020-02-27 17:22:16 -0500

    feat: pass nodeSelector and tolerations to Argo Workflows

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-if [ $# -ne 3 ]
+if [ $# -ne 2 ]
 then
   echo "Illegal number of parameters. Exiting..."
-  echo "Command: ./entrypoint.sh \${mongo_host} \${mongo_port} \${shared_pvc}"
+  echo "Command: ./entrypoint.sh \${mongo_host} \${mongo_port}"
   exit 1
 fi
 
 MONGO_HOST=$1
 MONGO_PORT=$2
-SHARED_PVC=$3
 
 sed -i \
   -e 's/@mongo_host@/'"${MONGO_HOST}"'/' \
   -e 's/@mongo_port@/'"${MONGO_PORT}"'/' \
   -e 's/@shared_pvc@/'"${SHARED_PVC}"'/' \
+  -e 's|@workflow_nodeSelector@|'"${NODE_SELECTOR}"'|' \
+  -e 's|@workflow_tolerations@|'"${TOLERATIONS}"'|' \
   /opt/wipp/config/application.properties
 
 if [[ -n ${ELASTIC_APM_SERVER_URLS} && -n ${ELASTIC_APM_SERVICE_NAME} ]]; then
