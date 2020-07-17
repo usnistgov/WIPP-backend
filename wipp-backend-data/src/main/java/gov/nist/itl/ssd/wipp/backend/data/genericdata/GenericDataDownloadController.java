a35b195fedb825597f7edd5e10c34c4d45e916f2
mohamedOuladi <mohamed.ouladi@nist.gov>
2020-07-17 10:06:18 -0400

    feat: added generic data data type

---
+++ wipp-backend-data/src/main/java/gov/nist/itl/ssd/wipp/backend/data/genericdata/GenericDataDownloadController.java
@@ -0,0 +1,98 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of
+ * their official duties. Pursuant to title 17 Section 105 of the United
+ * States Code this software is not subject to copyright protection and is
+ * in the public domain. This software is an experimental system. NIST assumes
+ * no responsibility whatsoever for its use by other parties, and makes no
+ * guarantees, expressed or implied, about its quality, reliability, or
+ * any other characteristic. We would appreciate acknowledgement if the
+ * software is used.
+ */
+package gov.nist.itl.ssd.wipp.backend.data.genericdata;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import gov.nist.itl.ssd.wipp.backend.core.CoreConfig;
+
+/**
+*
+* @author Mohamed Ouladi <mohamed.ouladi at nist.gov>
+*/
+@Controller
+@RequestMapping(CoreConfig.BASE_URI + "/genericDatas/{genericDataId}/download")
+public class GenericDataDownloadController {
+
+	@Autowired
+	CoreConfig config;
+
+	@Autowired
+	GenericDataRepository genericDataRepository;
+
+	@RequestMapping(
+			value = "",
+			method = RequestMethod.GET,
+			produces = "application/zip")
+	public void get(
+			@PathVariable("genericDataId") String genericDataId,
+			HttpServletResponse response) throws IOException {
+		
+		GenericData genericData = null;
+		Optional<GenericData> optGenericData = genericDataRepository.findById(genericDataId);
+		
+		if (!optGenericData.isPresent()) {
+			throw new ResourceNotFoundException(
+					"Generic Data " + genericDataId + " not found.");
+		} else { // generic data is present
+			genericData = optGenericData.get();
+        }
+
+		// get generic data folder
+		File genericDataStorageFolder = new File(config.getGenericDatasFolder(), genericData.getId());
+		if (! genericDataStorageFolder.exists()) {
+			throw new ResourceNotFoundException(
+					"Generic data " + genericDataId + " " + genericData.getName() + " not found.");
+		}
+
+		response.setHeader("Content-disposition",
+				"attachment;filename=" + "GenericData-" + genericData.getName() + ".zip");
+
+		ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
+		addToZip("", zos, genericDataStorageFolder);
+		zos.finish();
+	}
+
+	//Recursive method to handle sub-folders
+	public static void addToZip(String path, ZipOutputStream myZip, File f) throws FileNotFoundException, IOException{
+		if(f.isDirectory()){
+			for(File subF : f.listFiles()){
+				addToZip(path + File.separator + f.getName() , myZip, subF);
+			}
+		}
+		else {
+			ZipEntry e = new ZipEntry(path + File.separator + f.getName());
+			myZip.putNextEntry(e);
+			try (InputStream is = new FileInputStream(f.getAbsolutePath())) {
+				IOUtils.copyLarge(is, myZip);
+			}
+		}
+	}
+	
+}
