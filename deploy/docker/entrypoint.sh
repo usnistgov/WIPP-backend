2acfd2215c45bb2afe7ee6b59b40587a5efde26e
Mylene Simon <mylene.simon@nist.gov>
2020-09-24 21:37:09 -0400

    fix: sed keycloak config

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -23,6 +23,8 @@ sed -i \
   -e 's/@mongo_host@/'"${MONGO_HOST}"'/' \
   -e 's/@mongo_port@/'"${MONGO_PORT}"'/' \
   -e 's/@shared_pvc@/'"${SHARED_PVC}"'/' \
+  -e 's|@keycloak_auth_url@|'"${KEYCLOAK_AUTH_URL}"'|' \
+  -e 's|@keycloak_ssl_required@|'"${KEYCLOAK_SSL_REQUIRED}"'|' \
   -e 's|@workflow_nodeSelector@|'"${NODE_SELECTOR}"'|' \
   -e 's|@workflow_tolerations@|'"${TOLERATIONS}"'|' \
   /opt/wipp/config/application.properties
